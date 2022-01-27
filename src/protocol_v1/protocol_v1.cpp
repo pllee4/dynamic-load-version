8f09f875321f9dcfc5cd6b93b331b91336254dbb
Pin Loon Lee <pinloon_0428@hotmail.com>
2022-01-27 21:29:41 +0800

    src: protocol_v*: added support for Windows

--- src/protocol_v1/protocol_v1.cpp
+++ src/protocol_v1/protocol_v1.cpp
@@ -10,11 +10,21 @@
 
 #include <iostream>
 
+#if defined(__linux__) || defined(__APPLE__)
 extern "C" {
 ProtocolV1 *allocator() { return new ProtocolV1(); }
 
 void deleter(ProtocolV1 *ptr) { delete ptr; }
 }
+#endif
+
+#ifdef WIN32
+extern "C" {
+__declspec(dllexport) ProtocolV1 *allocator() { return new ProtocolV1(); }
+
+__declspec(dllexport) void deleter(ProtocolV1 *ptr) { delete ptr; }
+}
+#endif
 
 void ProtocolV1::Initialize() {
   std::cout << "Calling Initialize from ProtocolV1" << std::endl;
--- src/protocol_v2/protocol_v2.cpp
+++ src/protocol_v2/protocol_v2.cpp
@@ -1,20 +1,30 @@
-/* 
+/*
  * protocol_v2.cpp
  * Created on: Jan 25, 2022 22:24
- * Description: 
- * 
+ * Description:
+ *
  * Copyright (c) 2022 Pin Loon Lee (pllee4)
- */ 
+ */
 
 #include "protocol_v2.hpp"
 
 #include <iostream>
 
+#if defined(__linux__) || defined(__APPLE__)
 extern "C" {
 ProtocolV2 *allocator() { return new ProtocolV2(); }
 
 void deleter(ProtocolV2 *ptr) { delete ptr; }
 }
+#endif
+
+#ifdef WIN32
+extern "C" {
+__declspec(dllexport) ProtocolV2 *allocator() { return new ProtocolV2(); }
+
+__declspec(dllexport) void deleter(ProtocolV2 *ptr) { delete ptr; }
+}
+#endif
 
 void ProtocolV2::InitializeV2() {
   std::cout << "Calling Initialize from ProtocolV2" << std::endl;
